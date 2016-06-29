b701b5f2ae9519e3b4b6257df02d5c6184f0c033
zhangxiaoyang <zhangxiaoyang.hit@gmail.com>
2016-06-29 14:01:13 +0000

    adjust magic

--- WechatImageDecoder.py
+++ WechatImageDecoder.py
@@ -23,11 +23,10 @@ class WechatImageDecoder:
         return decoders[None]
 
     def _decode_pc_dat(self, datfile):
-        magic = 0x51
-
         with open(datfile, 'rb') as f:
             buf = bytearray(f.read())
 
+        magic = 0xff ^ list(buf)[0] if buf else 0x00
         imgfile = re.sub(r'.dat$', '.jpg', datfile)
         with open(imgfile, 'wb') as f:
             newbuf = bytearray([b ^ magic for b in list(buf)])
